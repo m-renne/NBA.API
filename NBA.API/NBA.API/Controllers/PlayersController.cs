6440e227b56ab97d631d7ffee9f1eecc081e9489
Matt Renne <Matt Renne>
2017-03-26 11:33:11 -0500

    Adds Initial Project

---
+++ NBA.API/NBA.API/Controllers/PlayersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NBA.API.Controllers
+{
+    public class PlayersController : ApiController
+    {
+        [Route("players")]
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            var players = new { test = 1 };
+
+            return Request.CreateResponse(HttpStatusCode.OK, players);
+        }
+    }
+}

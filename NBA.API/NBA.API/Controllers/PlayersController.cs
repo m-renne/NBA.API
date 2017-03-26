0022db3e6e648bf71986146f0474124f3b4f829f
Matt Renne <Matt Renne>
2017-03-26 12:04:52 -0500

    test

--- NBA.API/NBA.API/Controllers/PlayersController.cs
+++ NBA.API/NBA.API/Controllers/PlayersController.cs
@@ -13,7 +13,7 @@ namespace NBA.API.Controllers
         [HttpGet]
         public HttpResponseMessage Get()
         {
-            var players = new { test = 4};
+            var players = new { test = 5};
 
             return Request.CreateResponse(HttpStatusCode.OK, players);
         }
